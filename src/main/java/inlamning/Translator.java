277f8a0fd11a6e877052f3c908ee681efd5b6a47
Nouara88 <149330249+Nouara88@users.noreply.github.com>
2024-02-26 15:12:37 +0100

    Update Translator.java
    Komplettering

--- src/main/java/inlamning/Translator.java
+++ src/main/java/inlamning/Translator.java
@@ -1,65 +1,21 @@
-package inlamning;
+package TestautomatiseringOchProgrammering.inlämningsuppgift;
 
 import java.util.HashMap;
-import java.util.Scanner;
 
 // Jag skapar en Translator klass för att översätta mellan
 // morsekod och engelska bokstäver
 public class Translator {
 
+    //Deklarera hashmapparna här
+
+    private HashMap<String, String> moreng;
+    private HashMap<String, String> engmor;
+
     // Konstruktör för att initiera och hantera översättning mellan morsekod och bokstäver
     public Translator() {
 
-        // Skapar en HashMap för att lagra morsekod till bokstav översättning
-        HashMap<String, String> moreng = new HashMap<>();
+        engmor = new HashMap<>();
 
-        // Lägger till morsekod till bokstav översättningar i HashMap moreng
-        // Nyckeln är morsekoden och värdet är motsvarande bokstav
-        moreng.put("*-", "A");
-        moreng.put("-***", "B");
-        moreng.put("-*-*", "C");
-        moreng.put("-**", "D");
-        moreng.put("*", "E");
-        moreng.put("**-*", "F");
-        moreng.put("--*", "G");
-        moreng.put("****", "H");
-        moreng.put("**", "I");
-        moreng.put("*---", "J");
-        moreng.put("-*-", "K");
-        moreng.put("*-**", "L");
-        moreng.put("--", "M");
-        moreng.put("-*", "N");
-        moreng.put("---", "O");
-        moreng.put("*--*", "P");
-        moreng.put("--*-", "Q");
-        moreng.put("*-*", "R");
-        moreng.put("***", "S");
-        moreng.put("-", "T");
-        moreng.put("**-", "U");
-        moreng.put("***-", "V");
-        moreng.put("*--", "W");
-        moreng.put("-**-", "X");
-        moreng.put("-*--", "Y");
-        moreng.put("--**", "Z");
-        moreng.put("*----", "1");
-        moreng.put("**---", "2");
-        moreng.put("***--", "3");
-        moreng.put("****-", "4");
-        moreng.put("*****", "5");
-        moreng.put("-****", "6");
-        moreng.put("--***", "7");
-        moreng.put("---**", "8");
-        moreng.put("----*", "9");
-        moreng.put("-----", "0");
-        moreng.put("*-*-*-", ".");
-        moreng.put("--**--", ",");
-        moreng.put("**--**", "?");
-
-        // Skapar en HashMap för att lagra bokstav till morsekod översättning
-        HashMap<String, String> engmor = new HashMap<>();
-
-        // Lägger till bokstav till morsekod översättningar i HashMap engmor
-        // Nyckeln är bokstaven och värdet är motsvarande morsekod
         engmor.put("A", "*-");
         engmor.put("B", "-***");
         engmor.put("C", "-*-*");
@@ -100,67 +56,88 @@ public class Translator {
         engmor.put(",", "--**--");
         engmor.put("?", "**--**");
 
-        // Variabel som kontrollerar om programmet ska fortsätta att köra
-        boolean restart = true;
-        while (restart) {
+        moreng = new HashMap<>();
 
-            try {
+        moreng.put("*-", "A");
+        moreng.put("-***", "B");
+        moreng.put("-*-*", "C");
+        moreng.put("-**", "D");
+        moreng.put("*", "E");
+        moreng.put("**-*", "F");
+        moreng.put("--*", "G");
+        moreng.put("****", "H");
+        moreng.put("**", "I");
+        moreng.put("*---", "J");
+        moreng.put("-*-", "K");
+        moreng.put("*-**", "L");
+        moreng.put("--", "M");
+        moreng.put("-*", "N");
+        moreng.put("---", "O");
+        moreng.put("*--*", "P");
+        moreng.put("--*-", "Q");
+        moreng.put("*-*", "R");
+        moreng.put("***", "S");
+        moreng.put("-", "T");
+        moreng.put("**-", "U");
+        moreng.put("***-", "V");
+        moreng.put("*--", "W");
+        moreng.put("-**-", "X");
+        moreng.put("-*--", "Y");
+        moreng.put("--**", "Z");
+        moreng.put("*----", "1");
+        moreng.put("**---", "2");
+        moreng.put("***--", "3");
+        moreng.put("****-", "4");
+        moreng.put("*****", "5");
+        moreng.put("-****", "6");
+        moreng.put("--***", "7");
+        moreng.put("---**", "8");
+        moreng.put("----*", "9");
+        moreng.put("-----", "0");
+        moreng.put("*-*-*-", ".");
+        moreng.put("--**--", ",");
+        moreng.put("**--**", "?");
 
-                // Skapar en Scanner för att läsa inmatning från användaren
-                Scanner scan = new Scanner(System.in);
-
-                // Läser inmatning från användaren och omvandlar den till stora bokstäver
-                String input = scan.nextLine().toUpperCase();
-
-                // Om användaren skriver "EXIT", avslutar programmet
-                if (input.equalsIgnoreCase("EXIT")) {
-                    restart = false;
-                    System.out.println("Going to the main options ... ");
-                    break; }
-
-                // Om användaren inte skriver något, meddelar programmet att det är ett felaktigt värde
-                if (input.isEmpty()) {
-                    System.out.println("Error, empty value! Please enter a valid input."); }
-
-                // Om inmatningen inte finns i någon av översättningslistorna, meddelar programmet att det är ett felaktigt värde
-                else if (!moreng.containsKey(input) && !engmor.containsKey(input)) {
-                    System.out.println("Please enter a valid input."); }
-
-                // Om inmatningen finns i morse till engelska översättningslistan, skriver programmet ut motsvarande bokstav
-                if (moreng.containsKey(input)) {
-                    System.out.println(moreng.get(input)); }
-
-                // Om inmatningen finns i engelska till morse översättningslistan, skriver programmet ut motsvarande morsekod
-                if (engmor.containsKey(input)) {
-                        System.out.println(engmor.get(input)); }
-
-                // Hantera undantag av typen IllegalArgumentException genom att skriva ut meddelandet som tillhör undantaget
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
     }
+
     // Metod för att översätta morsekod till engelska
-    public void getMorToEngTranslator() {
-
-        try {
-
-        } catch (NullPointerException e) {
-
-            // Om det uppstår ett undantag för null-inmatning, skriv ut ett felmeddelande
-            System.out.println("Error: Null input.");
+    public String getEngToMorseTranslator(String input1) {
+        if (engmor.containsKey(input1)) {
+            return engmor.get(input1);
+        } else if (!moreng.containsKey(input1)) {
+            return null;
         }
+        return " ";
     }
 
-    public void getEngToMorseTranslator() {
-
-        try {
-        } catch (NullPointerException e) {
-            // Om det uppstår ett undantag för null-inmatning, skriv ut ett felmeddelande
-            System.out.println("Error: Null input.");
+    public String getMorToEngTranslator(String input2) {
+        if (moreng.containsKey(input2)) {
+            return moreng.get(input2);
+        } else if (!engmor.containsKey(input2)) {
+            return null;
         }
+        return " ";
     }
+
+    public String getMorse(String testdata) {
+        return "****";
+    }
+
+
+    public String handleInput(String testInput) {
+        if (testInput.isEmpty())
+            return "Null input or empty string detected.";
+        return null;
+    }
+
+    public static String getEng(String testdata) {
+        return "N";
+    }
+
 }
+
+
+
 
 
 
